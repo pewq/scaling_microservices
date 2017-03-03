a356ed6951512bc5aa58ea7696d0a83f6f3bd0a9
Dmytro <WhyPieDie>
2017-03-03 15:30:36 +0200

    moved to folder

---
+++ scaling_microservices/Registry/RegistryEntry.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scaling_microservices.Registry
+{
+    class RegistryEntry : IComparable
+    {
+        public string Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Token { get; set; } //unused
+
+        //TODO : maybe extract enum ServiceType
+        public string ServiceType { get; set; }
+
+        public DateTime? Expiry { get; private set; } //TODO : is this really needed?
+
+        static private bool EqualOrNone(string left, string right)
+        {
+            if (left == "" || right == "")
+                return true;
+            return left == right;
+        }
+
+        public RegistryEntry(int offset = ServiceRegistry.DefaultTimeout)
+        {
+            Expiry = DateTime.Now.AddSeconds(offset);
+        }
+
+        public RegistryEntry(DateTime expiryDate)
+        {
+            if(expiryDate < DateTime.Now)
+            {
+                throw new Exception("RegistryEntry(DateTime) : date of expiration should be bigger than current date");
+            }
+            Expiry = expiryDate;
+        }
+
+        public void Reset(int offset = ServiceRegistry.DefaultTimeout)
+        {
+            Expiry = DateTime.Now.AddSeconds(offset);
+        }
+
+        #region IComparable
+
+        //for sorting by expiration time
+        public int CompareTo(object obj)
+        {
+            try
+            {
+                return Expiry.Value.CompareTo((obj as RegistryEntry).Expiry.Value);
+            }
+            catch(InvalidCastException)
+            {
+                throw new ArgumentException("cannot cast to RegistryEntry");
+            }
+            catch(InvalidOperationException)
+            {
+                throw new ArgumentException("object has no value");
+            }
+        }
+
+        #endregion IComparable
+        #region Equals
+
+        public static bool operator == (RegistryEntry first, RegistryEntry second)
+        {
+            return (first.Id == second.Id && first.Address == second.Address) &&           
+                EqualOrNone(first.ServiceType, second.ServiceType) &&
+                EqualOrNone(first.Token, second.Token);
+        }
+
+        public static bool operator != (RegistryEntry first, RegistryEntry second)
+        {
+            return !(first == second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null) || ReferenceEquals(this, null))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            try
+            {
+                return this == (obj as RegistryEntry);
+            }
+            catch(InvalidCastException)
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return ServiceType.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ scaling_microservices/Registry/ServiceRegistry.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using System.Linq;
+
+namespace scaling_microservices.Registry
+{
+    /// <summary>
+    /// class, which contains active services and recieves
+    /// pings from them in order to keep the connection
+    /// </summary>
+    class ServiceRegistry
+    {
+        private List<RegistryEntry> items;
+
+        private List<RegistryEntry> candidates;
+
+        private Timer registerTimer;
+
+        private void RecalculateTimer()
+        {
+            if(items.Count == 0)
+            {
+                registerTimer.Enabled = false;
+                return;
+            }
+            DateTime expiryTime = items.Min().Expiry.Value;
+            candidates = items.Where(x => x.Expiry.Value == expiryTime).ToList();
+            registerTimer.Enabled = true;
+            registerTimer.Interval = expiryTime.Subtract(DateTime.Now).TotalMilliseconds;
+        }
+        
+        public int timeout { get; }
+
+        public ServiceRegistry(int timeoutInSeconds = ServiceRegistry.DefaultTimeout)
+        {
+            timeout = timeoutInSeconds;
+            items = new List<RegistryEntry>();
+            candidates = new List<RegistryEntry>();
+            registerTimer = new Timer() { AutoReset = true, Enabled = false };
+            registerTimer.Elapsed += (object sender, ElapsedEventArgs e) =>
+            {
+                foreach (var can in candidates)
+                {
+                    items.Remove(can);
+                }
+                this.RecalculateTimer();
+            };
+        }
+
+        public const int DefaultTimeout = 60;
+
+        public void Ping(string id, string token)
+        {
+            token = "";//todo : implement service tokens
+            var item = items.Find( x => 
+                ( x == new RegistryEntry() { Id = id, Token = token })
+            );
+            item.Reset();
+        }
+
+        public void Add(string id, string address, string token, string type)
+        {
+            RegistryEntry entry = new RegistryEntry()
+                { Id = id, Address = address, Token = token, ServiceType = type };
+            //todo : maybe change later to this.Contains
+            RegistryEntry item = items.Find(x => x == entry);
+            if(item != null)
+            {
+                item.Reset();
+            }
+            else
+            {
+                items.Add(entry);
+                entry.Reset();
+            }
+            this.RecalculateTimer();
+        }
+
+        //add ContainsId, ContainsAddress etc. maybe general Contains with return type enum
+        private bool Contains(RegistryEntry entry)
+        {
+            return false;
+        }
+
+        public Dictionary<string,DateTime> Get()
+        {
+            return items.Where(x => x.Expiry.HasValue)
+                .ToDictionary(x => x.Id, x => x.Expiry.Value);
+        }
+
+        public List<string> GetServices()
+        {
+            return items.Select(x => x.Id).ToList();
+        }
+
+
+    }
+}
+
+//todo : add 3rdp-like registration behaviour
+
+//example : 
+/*
+ * int timeout = 100;
+ * var registry = new ServiceRegistry(timeout);
+ * registry.Add("service-token");
+ * registry.Get().Count; // == 1
+ * Thread.Wait((timeout + 10) * 1000); //110 seconds;
+ * registry.Get().Count; // == 0
+ */
