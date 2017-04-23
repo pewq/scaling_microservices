79d89405020a92626543d91dd6bad597b2822c02
WhyPieDie <holydarkness0134@gmail.com>
2017-04-24 02:14:45 +0300

    added auth handle template

--- auth_service/AuthService.cs
+++ auth_service/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using scaling_microservices.Rabbit;
+﻿using System;
+using scaling_microservices.Rabbit;
 
 namespace auth_service
 {
@@ -19,6 +20,59 @@ namespace auth_service
         private void ThisInit()
         {
             //Init db here;
+        }
+
+        private string handleBasicAuth(string token)
+        {
+            return "";
+        }
+        private string handleCredentialAuth(string login, string password)
+        {
+            return "";
+        }
+
+        private void AuthenticationHandler(QueueRequest req)
+        {
+            try
+            {
+                string authType = req["authentication"];
+                string retValue = "";
+                switch(authType.ToLower())
+                {
+                    case "basic":
+                        {
+                            retValue = handleBasicAuth(req["token"]);
+                            break;
+                        }
+                    case "credential":
+                        {
+                            retValue = handleCredentialAuth(req["logoin"], req["password"]);
+                            break;
+                        }
+                    default:
+                        {
+                            throw new Exception("Invalid authentication type");
+                        }
+                }
+                OnResponse(req.properties, retValue);
+            }
+            catch(Exception e)
+            {
+                OnException(e, req);
+            }
+        }
+
+        private void AuthorizationHandler(QueueRequest req)
+        {
+            try
+            {
+                var success = authenticate(req["token"]);
+                OnResponse(req.properties, success);
+            }
+            catch (Exception e)
+            {
+                OnException(e, req);
+            }
         }
     }
 }
