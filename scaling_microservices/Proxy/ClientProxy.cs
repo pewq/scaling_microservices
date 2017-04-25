daab56cdf56478fbf841a5e53722b0ea001ae481
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 02:12:31 +0300

    updated client proxy

--- scaling_microservices/Proxy/ClientProxy.cs
+++ scaling_microservices/Proxy/ClientProxy.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Model;
+using scaling_microservices.Proxy.Model;
+using scaling_microservices.Rabbit;
+using Newtonsoft.Json;
 
 namespace scaling_microservices.Proxy
 {
@@ -11,6 +13,52 @@ namespace scaling_microservices.Proxy
     {
         public ClientProxy(string routing = "", string exchange = "") : base(routing, exchange) { }
 
-        public List<UserModel>
+        public List<UserModel> GetUsers()
+        {
+            var req = new QueueRequest() { method = "get_users" };
+            Send(req);
+            var response = endpoint.Recieve();
+            var result = JsonConvert.DeserializeObject<List<UserModel>>(response.StringBody);
+            return result;
+        }
+        public UserModel GetUser(int userId = 0, string userName = "", string userEmail = "")
+        {
+            var req = new QueueRequest() { method = "get_user" };
+            req["user_id"] = userId.ToString();
+            req["user_name"] = userName;
+            req["user_email"] = userEmail;
+            Send(req);
+            return JsonConvert.DeserializeObject<UserModel>(endpoint.Recieve().StringBody);
+        }
+
+        public bool AddUser(UserModel user)
+        {
+            var req = new QueueRequest() { method = "add_user" };
+            req["user"] = JsonConvert.SerializeObject(user);
+            Send(req);
+            return JsonConvert.DeserializeObject<bool>(endpoint.Recieve().StringBody);
+        }
+
+        public bool EditUser(int userId, string userName = "", string userEmail = "")
+        {
+            if(userName == "" && userEmail == "")
+            {
+                return false;
+            }
+            var req = new QueueRequest() { method = "edit_user" };
+            req["user_id"] = userId.ToString();
+            req["user_name"] = userName;
+            req["user_email"] = userEmail;
+            Send(req);
+            return JsonConvert.DeserializeObject<bool>(endpoint.Recieve().StringBody);
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            var req = new QueueRequest() { method = "delete_user" };
+            req["user_id"] = userId.ToString();
+            Send(req);
+            return JsonConvert.DeserializeObject<bool>(endpoint.Recieve().StringBody);
+        }
     }
 }
