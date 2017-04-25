54bec7df770c2bed34fc3777a8a7ccab267ce5a6
WhyPieDie <holydarkness0134@gmail.com>
2017-04-25 20:16:15 +0300

    test_project changes

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using scaling_microservices.Rabbit;
-using RabbitMQ.Client;
-using discovery_service;
+using scaling_microservices.Auth.Tokens;
+using StackExchange.Redis;
 
 namespace test_project
 {
@@ -13,11 +8,16 @@ namespace test_project
     {
         static void Main(string[] args)
         {
-//            var service = new DiscoveryService(DIs);
-            var proxy = new scaling_microservices.Proxy.DiscoveryProxy(DiscoveryService.QueueName);
-            proxy.Register("main", "addr", "erwer", "type t");
-            proxy.Register("main1", "addr1", "erwer1", "type t1");
-            proxy.Ping("main", "erwer");
+            RedisKeyValueStorage storage = new RedisKeyValueStorage();
+            var a = storage.Set("pewpew", "qwe", 30);
+            Console.WriteLine(storage.Get("pewpew").Result);
+            Console.WriteLine(storage.Get("asd").Result);
+            //var auth = new AuthService("authservice");
+            //DiscoveryService.Instance.GetType();
+            //var proxy = new scaling_microservices.Proxy.DiscoveryProxy(DiscoveryService.QueueName);
+            //proxy.Register("main", "addr", "erwer", "type t");
+            //proxy.Register("main1", "addr1", "erwer1", "type t1");
+            //proxy.Ping("main", "erwer");
             
             Console.ReadLine();
 
