b4cc384793feb3989353620a76d6e1e89f1650c4
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 02:11:12 +0300

    moved IsEqualOrNone method to helper class

---
+++ scaling_microservices/Helpers.cs
@@ -0,0 +1,14 @@
+﻿namespace scaling_microservices
+{
+    public class Helpers
+    {
+        private Helpers() { }
+
+        static public bool EqualOrNone(string left, string right)
+        {
+            if (left == "" || right == "")
+                return true;
+            return left == right;
+        }
+    }
+}
