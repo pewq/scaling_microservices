8a1cffc0e3d04660a22b37bbd1f31cbf15b14049
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 01:28:48 +0300

    added more proxies

---
+++ scaling_microservices/Proxy/ClientProxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace scaling_microservices.Proxy
+{
+    public class ClientProxy : IProxy
+    {
+        public ClientProxy(string routing = "", string exchange = "") : base(routing, exchange) { }
+
+        public List<UserModel>
+    }
+}
---
+++ scaling_microservices/Proxy/GroupProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scaling_microservices.Proxy
+{
+    class GroupProxy : IProxy
+    {
+        GroupProxy(string route = "", string exchange = "") : base(route, exchange) { }
+
+        
+    }
+}
