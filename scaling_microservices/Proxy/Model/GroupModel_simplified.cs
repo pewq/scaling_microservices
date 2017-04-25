9a5e24fd4930aa93d395a553d74098301c05f6a3
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 02:11:30 +0300

    added simplified model for GroupModel

---
+++ scaling_microservices/Proxy/Model/GroupModel_simplified.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace scaling_microservices.Proxy.Model
+{
+    public class GroupModel_simplified
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public int Creator { get; set; }
+        public Dictionary<int, int> Participants { get; set; } = new Dictionary<int, int>();
+    }
+}
