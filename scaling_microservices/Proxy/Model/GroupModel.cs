c770964108b4abe0ee8f7ba058af14cfd74b57e0
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 01:28:25 +0300

    added models for proxies

---
+++ scaling_microservices/Proxy/Model/GroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scaling_microservices.Proxy.Model
+{
+    public class GroupModel
+    {
+        string Name { get; set; }
+        string Owner { get; set; }
+        UserModel Creator { get; set; }
+        Dictionary<UserModel, RoleModel> Participants { get; set; } = new Dictionary<UserModel, RoleModel>();
+    }
+}
---
+++ scaling_microservices/Proxy/Model/RoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scaling_microservices.Proxy.Model
+{
+    public class RoleModel
+    {
+        string Name { get; set; }
+        string Owner { get; set; }
+        List<UserModel> Participants { get; set; } = new List<UserModel>();
+    }
+}
