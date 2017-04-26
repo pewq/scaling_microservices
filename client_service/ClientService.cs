9c04c311d351c8891f3c73b7d5f44a9c4cb5cf83
WhyPieDie <holydarkness0134@gmail.com>
2017-04-26 19:33:06 +0300

    updated client service

--- client_service/ClientService.cs
+++ client_service/ClientService.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using scaling_microservices.Proxy.Model;
+using scaling_microservices.StorageStub;
 using scaling_microservices.Rabbit;
 
 namespace client_service
@@ -20,6 +24,11 @@ namespace client_service
         private void ThisInit()
         {
             Handlers.Add("register_discovery", (RequestHandleDelegate)discoveryRegisterHandler);
+            Handlers.Add("get_users", (RequestHandleDelegate)GetUsersHandler);
+            Handlers.Add("get_user", (RequestHandleDelegate)SearchUserHandler);
+            Handlers.Add("add_user", (RequestHandleDelegate)AddUserHandler);
+            Handlers.Add("edit_user", (RequestHandleDelegate)EditUserHandler);
+            Handlers.Add("delete_user", (RequestHandleDelegate)DeleteUserHandler);
         }
         
         private bool CheckDiscoveryHealth()
@@ -45,6 +54,97 @@ namespace client_service
             }
         }
 
+        private void GetUsersHandler(QueueRequest req)
+        {
+            var users = UserStorage.storage.ToList();
+            OnResponse(req.properties, users);
+        }
+
+        private void SearchUserHandler(QueueRequest req)
+        {
+            var users = UserStorage.storage.ToList();
+            if (req["user_id"] != "0")
+            {
+                users = users.Where(x => x.UserId.ToString() == req["user_id"]).ToList();
+            }
+            if(req["user_name"] != "")
+            {
+                users = users.Where(x => x.Name == req["user_name"]).ToList();
+            }
+            if(req["user_email"] != "")
+            {
+                users = users.Where(x => x.Email == req["user_email"]).ToList();
+            }
+            OnResponse(req.properties, users.First());
+        }
+
+        private void AddUserHandler(QueueRequest req)
+        {
+            int resultId;
+            UserModel user = null;
+            try
+            {
+                user = JsonConvert.DeserializeObject<UserModel>(req["user"]);
+            }
+            catch (Exception e)
+            {
+                OnException(e, req);
+            }
+            if(user != null)
+            {
+                var existingUser = UserStorage.storage.GetById(user.UserId);
+                if(existingUser != null)
+                {
+                    resultId = -1;
+                }
+                else
+                {
+                    if(user.UserId != 0)
+                    {
+                        //Generate UserId
+                    }
+                    resultId = user.UserId;
+                }
+            }
+            else
+            {
+                resultId = 0;
+            }
+            OnResponse(req.properties, resultId);
+        }
+
+        private void EditUserHandler(QueueRequest req)
+        {
+            var user = UserStorage.storage.GetById(int.Parse(req["user_id"]));
+            if(user != null)
+            {
+                OnResponse(req.properties, false);
+                return;
+            }
+            if(req["user_name"] != "")
+            {
+                user.Name = req["user_name"];
+            }
+            if(req["user_email"] != "")
+            {
+                user.Email = req["user_email"];
+            }
+            //idk, whether user was set
+
+            OnResponse(req.properties, true);
+        }
+
+        private void DeleteUserHandler(QueueRequest req)
+        {
+            var user = UserStorage.storage.GetById(int.Parse(req["user_id"]));
+            if(user == null)
+            {
+                OnResponse(req.properties, false);
+                return;
+            }
+            UserStorage.storage.Remove(user);
+            OnResponse(req.properties, true);
+        }
         #endregion
 
     }
