267840950ed0809ef5a4e21c5e43adb80174bc2a
WhyPieDie <holydarkness0134@gmail.com>
2017-05-09 13:17:10 +0300

    added identityConfig class (it may be useless).

---
+++ auth_service/IdentityConfig.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using scaling_microservices.Identity;
+
+namespace auth_service
+{
+    class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new AppUserDbContext());
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<RoleManager<IdentityRole>>((options, context) =>
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context.Get<AppUserDbContext>())));
+        }
+    }
+}
