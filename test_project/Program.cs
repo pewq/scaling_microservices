82a3380c32b0bc2f511df610821bbd2995cf76f8
WhyPieDie <holydarkness0134@gmail.com>
2017-05-12 15:54:28 +0300

    test_project

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -4,17 +4,22 @@ using scaling_microservices.Proxy;
 using System.Linq;
 using RabbitMQ.Client;
 using scaling_microservices.Rabbit;
+using scaling_microservices.Entity;
+using scaling_microservices.Identity;
+using scaling_microservices.Model;
 
 namespace test_project
 {
+    using Newtonsoft.Json;
     class Program
     {
+        
         static void Main(string[] args)
         {
-            var endpoint = new EventingEndpoint();
-            var queueHandle = endpoint.channel.QueueDeclarePassive(queue: auth_service.AuthService.AuthServiceQueue);
-            Console.WriteLine(queueHandle.QueueName);
-            Console.ReadLine();
+            var str = JsonConvert.SerializeObject(null);
+            var template = new { msg = "" };
+            var obj = JsonConvert.DeserializeAnonymousType(str,  template);
+
         }
     }
 }
